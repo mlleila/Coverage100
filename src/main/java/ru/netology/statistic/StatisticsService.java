6dd5706cb9977e2a2d55decf721c08d6bc45ad62
Лейла <leila@MBP-Dmitrij.lan>
2020-12-20 14:01:05 +0300

    StatisticsServiceTest.java modified

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,11 +8,68 @@ public class StatisticsService {
    * @return - index of first max value
    */
   public long findMax(long[] incomes) {
-    long current_max_index = 0;
     long current_max = incomes[0];
     for (long income : incomes)
-      if (current_max < income)
-        current_max = income;
+      if (current_max < income) {
+        current_max = income;}
         return current_max;
+  }
+  public long calculateSum(long[] purchases) {
+    long sum = 0;
+    for (long purchase : purchases) {
+      sum += purchase;
+    }
+    return sum;
+  }
+  public long calculateAverageSum(long[] purchases){
+    return calculateSum(purchases) / 12;
+  }
+
+  public long numberOfMonthWithMaxPurchases(long[] purchases) {
+    long max = purchases[0];
+    long indexForMax = 0;
+    for (int i = 0; i < purchases.length; i++) {
+      long purchase = purchases[i];
+      if (max <= purchase) {
+        max = purchase;
+        indexForMax = i;
+      }
+    }
+    return indexForMax;
+  }
+
+  public long numberOfMonthWithMinPurchases(long[] purchases) {
+    long min = purchases[0];
+    long indexForMax = 0;
+    for (int i = 0; i < purchases.length; i++) {
+      long purchase = purchases[i];
+      if (min >= purchase) {
+        min = purchase;
+        indexForMax = i;
+      }
+    }
+    return indexForMax;
+  }
+
+  public long numberOfMonthsUnderAverageSum(long[] purchases) {
+    long months = 0;
+    for (long purchase : purchases) {
+      if (purchase < calculateAverageSum(purchases)) {
+        months++;
+      }
+    }
+    return months;
+  }
+
+  public long numberOfMonthsOverAverageSum(long[] purchases) {
+    long months = 0;
+    for (long purchase : purchases)
+    {
+      if (purchase > calculateAverageSum(purchases))
+      {
+        months++;
+      }
+    }
+    return months;
   }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,62 @@ class StatisticsServiceTest {
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
-
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
-
     assertEquals(expected, actual);
   }
+  @Test
+  void shouldCalculateSum() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 180;
+    long actual = service.calculateSum(purchases);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shouldCalculateAverageSum() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 15;
+    long actual = service.calculateAverageSum(purchases);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shouldCalculateNumberOfMonthWithMaxPurchases() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 7;
+    long actual = service.numberOfMonthWithMaxPurchases(purchases);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shouldCalculateNumberOfMonthWithMinPurchases() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 8;
+    long actual = service.numberOfMonthWithMinPurchases(purchases);
+    assertEquals(expected, actual);
+  }
+  @Test
+  void shouldCalculateNumberOfMonthsUnderAverageSum() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 5;
+    long actual = service.numberOfMonthsUnderAverageSum(purchases);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shouldCalculateNumberOfMonthsOverAverageSum() {
+    StatisticsService service = new StatisticsService();
+    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long expected = 5;
+    long actual = service.numberOfMonthsOverAverageSum(purchases);
+    assertEquals(expected, actual);
+  }
+
 }
